8fff88c9f3c8474ddc227c8893b8087eacc1806a
Gaorlabs <farmacia.erp.pe@gmail.com>
2025-12-09 17:18:34 -0500

    Add files via upload

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,1 +1,7 @@
 
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://aeideybonufqzpzmuhmt.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlaWRleWJvbnVmcXpwem11aG10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyMzgyNDMsImV4cCI6MjA4MDgxNDI0M30.ez_ZlF0RZua3gyo6TU1nO3rdznbYyuQIWtQjrmHhliU';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
