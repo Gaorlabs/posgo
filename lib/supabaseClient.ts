8fe16c8132064058abc148a90d13f7278527dad8
Gaorlabs <farmacia.erp.pe@gmail.com>
2025-12-11 21:13:54 -0500

    Delete lib directory

--- lib/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://aeideybonufqzpzmuhmt.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlaWRleWJvbnVmcXpwem11aG10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyMzgyNDMsImV4cCI6MjA4MDgxNDI0M30.ez_ZlF0RZua3gyo6TU1nO3rdznbYyuQIWtQjrmHhliU';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
